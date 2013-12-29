251fc541ec097529a7156d3aad06df80c4449521
Mick England <england.mick@gmail.com>
2013-12-29 14:51:14 -0500

    Removed a line using float(raw_input())

--- inches2cm.py
+++ inches2cm.py
@@ -2,7 +2,5 @@ def inches_to_cm(inches):
     conversion = inches * 2.54
     print "There are %.2f centimeters in %.2f inches" % (conversion, inches)
 
-s = raw_input('Enter the number of inches you want to convert: ')
-ret = float(s)
-inches_to_cm(ret)
-
+n = float(raw_input('Enter the number of inches you want to convert: '))
+inches_to_cm(n)
