721662d81babc605a4e2f02f1f6ea0803ebca95c
Mick England <mick.england@digilant.com>
2014-03-21 09:58:41 -0400

    New NTP module.

---
+++ admin/manifests/ntp.pp
@@ -0,0 +1,18 @@
+# Manage NTP
+class admin::ntp {
+  package { 'ntp':
+    ensure  => installed,
+  }
+
+  service { 'ntp':
+    ensure  => running,
+    require => Package['ntp'],
+  }
+
+  file { '/etc/ntp.conf':
+    source  => 'puppet:///modules/admin/ntp.conf',
+    notify  => Service['ntp'],
+    require => Package['ntp'],
+  }
+}
+
---
+++ admin/spec/classes/ntp_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'admin::ntp' do
+    it { should contain_package('ntp') }
+    it { should contain_service('ntp').with('ensure' => 'running') }
+    it { should contain_file('/etc/ntp.conf') }
+end
